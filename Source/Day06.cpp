ccff680fe4004ad7214206466e0b4de4b87ebb71
JoeyManso <jmanso522@gmail.com>
2021-12-06 12:28:14 -0600

    Completion of Day 6

--- Source/Day06.cpp
+++ Source/Day06.cpp
@@ -1,14 +1,43 @@
 #include "Days.h"
+#include "AdventUtils.h"
+#include <vector>
 
 using namespace std;
 
+const int TIMER_RESET = 7;
+const int TIMER_BIRTH = 9;
+
 template <>
 void Run<Day06>(Part part, istream& is, ostream& os)
-{
-	if(part == Part01)
+{	
+	string arg;
+	getline(is, arg);
+	vector<int> lanternFishTimers = splitInt(arg, ',');
+	
+	// Gather seed timers, incrementing the respective index
+	long long fishCountPerTimers[TIMER_BIRTH] = { 0 };
+	for (int timer : lanternFishTimers)
 	{
+		++fishCountPerTimers[timer];
 	}
-	else if(part == Part02)
+	
+	const int simulationDays = part == Part01 ? 80 : 256;
+	long long totalFish = lanternFishTimers.size();
+	
+	// Run simulation for required days
+	// At each timer step, migrate fish counts and add spawned fish
+	for (int day = 0; day < simulationDays; ++day)
 	{
+		long long spawnedFish = fishCountPerTimers[0];
+		for (int i = 0; i < TIMER_BIRTH - 1; ++i)
+		{
+			fishCountPerTimers[i] = fishCountPerTimers[i + 1];
+		}
+		
+		// Add newly spawned fish
+		fishCountPerTimers[TIMER_RESET - 1] += spawnedFish;
+		fishCountPerTimers[TIMER_BIRTH - 1] = spawnedFish;
+		totalFish += spawnedFish;
 	}
+	cout << "Lanternfish after " << simulationDays << " days: " << totalFish << endl;
 }
